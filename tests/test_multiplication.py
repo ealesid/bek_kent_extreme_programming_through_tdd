047c6607fa9073b4c8472a456794530c1e578a49
ealesid <ealesid@gmail.com>
2019-12-19 22:19:29 +0300

    tests: one more assertion

--- tests/test_multiplication.py
+++ tests/test_multiplication.py
@@ -4,6 +4,9 @@ from src.part1.dollar import Dollar
 def test_multiplication():
 
     five_dollars = Dollar(5)
-    five_dollars.times(2)
 
+    five_dollars.times(2)
     assert five_dollars.amount == 5 * 2
+
+    five_dollars.times(3)
+    assert five_dollars.amount == 5 * 3
