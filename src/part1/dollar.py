b002053b18b8a39f53df491d8ad82f20d96c2786
ealesid <ealesid@gmail.com>
2019-12-20 13:29:55 +0300

    part1: class Franc

--- src/part1/dollar.py
+++ src/part1/dollar.py
@@ -12,3 +12,19 @@ class Dollar(object):
     @property
     def amount(self):
         return self.__amount
+
+
+class Franc(object):
+
+    def __init__(self, amount):
+        self.__amount = amount
+
+    def __eq__(self, other):
+        return self.amount == other.amount
+
+    def times(self, multiplier):
+        return Dollar(self.amount * multiplier)
+
+    @property
+    def amount(self):
+        return self.__amount
