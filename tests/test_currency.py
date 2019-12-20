853c6ff4107a557df6d7e66d049e8ebcda016dd9
ealesid <ealesid@gmail.com>
2019-12-20 15:06:44 +0300

    tests: Dollar replaced by Currency.dollar

--- tests/test_currency.py
+++ tests/test_currency.py
@@ -7,22 +7,22 @@ def test_multiplication():
 
     product = five_dollars.times(2)
     assert product.amount == 5 * 2
-    assert product == Dollar(5 * 2)
+    assert product == Currency.dollar(5 * 2)
 
     product = five_dollars.times(3)
     assert product.amount == 5 * 3
-    assert product == Dollar(5 * 3)
+    assert product == Currency.dollar(5 * 3)
 
 
 def test_equality():
 
-    assert Dollar(5) == Dollar(5)
-    assert Dollar(5) != Dollar(6)
+    assert Currency.dollar(5) == Currency.dollar(5)
+    assert Currency.dollar(5) != Currency.dollar(6)
 
     assert Franc(5) == Franc(5)
     assert Franc(5) != Franc(6)
 
-    assert Dollar(5) != Franc(5)
+    assert Currency.dollar(5) != Franc(5)
 
 
 def test_franc_multiplication():
