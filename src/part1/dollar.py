d470fbae76c5cbc7c7f28cf1b57f8471542905f2
ealesid <ealesid@gmail.com>
2019-12-20 13:41:55 +0300

    tests: Franc inherited from Currency

--- src/part1/dollar.py
+++ src/part1/dollar.py
@@ -17,17 +17,7 @@ class Dollar(Currency):
         return Dollar(self.amount * multiplier)
 
 
-class Franc(object):
-
-    def __init__(self, amount):
-        self.__amount = amount
-
-    def __eq__(self, other):
-        return self.amount == other.amount
+class Franc(Currency):
 
     def times(self, multiplier):
-        return Dollar(self.amount * multiplier)
-
-    @property
-    def amount(self):
-        return self.__amount
+        return Franc(self.amount * multiplier)
