34e9e32cc74846a2859403aafbff230c00f1f33f
ealesid <ealesid@gmail.com>
2019-12-20 13:40:37 +0300

    tests: move Dollar.__eq__ to Currency

--- src/part1/dollar.py
+++ src/part1/dollar.py
@@ -3,18 +3,18 @@ class Currency(object):
     def __init__(self, amount):
         self._amount = amount
 
-
-class Dollar(Currency):
-
     def __eq__(self, other):
         return self.amount == other.amount
-
-    def times(self, multiplier):
-        return Dollar(self.amount * multiplier)
 
     @property
     def amount(self):
         return self._amount
+
+
+class Dollar(Currency):
+
+    def times(self, multiplier):
+        return Dollar(self.amount * multiplier)
 
 
 class Franc(object):
