a1944a2a9cc6eeb993688abcc65729472bba4e2c
ealesid <ealesid@gmail.com>
2019-12-21 14:20:06 +0300

    part1: currency unification

--- src/part1/currency.py
+++ src/part1/currency.py
@@ -1,7 +1,14 @@
-class Currency(object):
+from abc import ABC
+from abc import abstractmethod
 
-    def __init__(self, amount):
+
+class Currency(ABC):
+
+    _currency = None
+
+    def __init__(self, amount, currency):
         self._amount = amount
+        self._currency = currency
 
     def __eq__(self, other):
         return all([
@@ -9,26 +16,34 @@ class Currency(object):
             self.amount == other.amount
         ])
 
+    @abstractmethod
+    def times(self, multiplier):
+        pass
+
     @property
     def amount(self):
         return self._amount
 
+    @property
+    def currency(self):
+        return self._currency
+
     @staticmethod
     def dollar(amount):
-        return Dollar(amount)
+        return Dollar(amount, 'USD')
 
     @staticmethod
     def franc(amount):
-        return Franc(amount)
+        return Franc(amount, 'CHF')
 
 
 class Dollar(Currency):
 
     def times(self, multiplier):
-        return Dollar(self.amount * multiplier)
+        return Currency.dollar(self.amount * multiplier)
 
 
 class Franc(Currency):
 
     def times(self, multiplier):
-        return Franc(self.amount * multiplier)
+        return Currency.franc(self.amount * multiplier)
