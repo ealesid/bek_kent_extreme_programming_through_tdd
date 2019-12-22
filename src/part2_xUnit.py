98f94e8d37115eafc2d409a36fd3a9346b768b74
ealesid <ealesid@gmail.com>
2019-12-22 13:10:07 +0300

    class WasRun

---
+++ src/part2_xUnit.py
@@ -0,0 +1,12 @@
+class WasRun:
+    def __init__(self, name):
+        self.was_run = None
+
+    def test_method(self):
+        self.was_run = True
+
+
+test = WasRun('test_method')
+print(test.was_run)
+test.test_method()
+print(test.was_run)
