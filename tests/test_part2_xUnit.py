fa077d20671034e6e60b323dc91ee90e88f0d615
ealesid <ealesid@gmail.com>
2019-12-22 14:20:28 +0300

    accounting and control

--- tests/test_part2_xUnit.py
+++ tests/test_part2_xUnit.py
@@ -8,13 +8,14 @@ class TestCase:
     def teardown(self):
         pass
 
-    def run(self, result):
+    def run(self):
+        result = TestResult()
         result.test_started()
         self.setup()
-        exec('self.' + self.name + '()')
+        method = getattr(self, self.name)
+        method()
         self.teardown()
-        # method = getattr(self, self.name)
-        # method()
+        return result
 
 
 class WasRun(TestCase):
@@ -25,6 +26,9 @@ class WasRun(TestCase):
     def test_method(self):
         self.was_run = True
         self.log += ' test_method'
+
+    def test_broken_method(self):
+        raise Exception
 
     def setup(self):
         self.was_run = None
@@ -43,11 +47,36 @@ class TestCaseTest(TestCase):
     def test_template_method(self):
         test = WasRun('test_method')
         test.run()
+        print(f'\n\ttest.log >\t{test.log}')
         assert test.log == 'setup test_method teardown'
+
+    def test_result(self):
+        test = WasRun('test_method')
+        result = test.run()
+        print(f'\n\tresult.summary >\t{result.summary()}')
+        assert result.summary() == '1 run, 0 failed'
+
+    def test_failed_result(self):
+        test = WasRun('test_broken_method')
+        result = test.run()
+        assert result.summary() == '1 run, 1 failed'
 
     # def test_running(self):
     #     self.test.run()
     #     assert self.test.was_run
 
 
+class TestResult:
+    def __init__(self):
+        self.run_count = 0
+
+    def test_started(self):
+        self.run_count += 1
+
+    def summary(self):
+        return f'{self.run_count} run, 0 failed'
+
+
 TestCaseTest('test_template_method').run()
+TestCaseTest('test_result').run()
+TestCaseTest('test_failed_result').run()
